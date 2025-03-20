626ee828e4017da7f916661b6bba2846bab58c71
Karoki <wanjikukar19@gmail.com>
2025-03-20 12:47:54 +0300

    Initial

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ function renderBooks(books) {
   });
 }
 
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => {
+      renderBooks(data);
+      return data; // This line is optional but can be helpful for debugging
+    });
+}
+
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.textContent = book.name;
+    main.appendChild(h2);
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
